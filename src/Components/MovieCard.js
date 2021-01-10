377589755099cf34f9b57ca453d70250a4b5d3bb
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-10 13:59:23 -0500

    Added Nominate button

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MovieCard = ({movieInfo})=> {
-console.log(movieInfo)
+// console.log(movieInfo)
+    const [nomBtn, setNomBtn] = useState(false)
+
+    const changeStatus = () => {
+        const status = nomBtn
+        setNomBtn(!nomBtn)
+    }
 
     return (
         <>
             <li>{movieInfo.Title}, {movieInfo.Year}</li>
-            <button>Nominate</button>
+            <button onClick={() => changeStatus()}>
+                {nomBtn ? "Nominated" : "Nominate"}
+            </button>
         </>
     )
 }
