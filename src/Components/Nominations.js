86512bd74cf9df3eea1420a02ab59a739ebd85d3
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-12 22:25:24 -0500

    Added Nominate button

--- src/Components/Nominations.js
+++ src/Components/Nominations.js
@@ -9,7 +9,6 @@ const Nominations = (props)=> {
                 <li>Nomination2</li>
                 <li>Nomination3</li>
                 <li>Nomination4</li>
-                <li>Nomination5</li>
 
             </ul>
         </div>
