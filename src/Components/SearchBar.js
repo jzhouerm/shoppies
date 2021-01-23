78adeb6d0c39629ac4b64a638f1c27980e1ddf63
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-23 01:22:07 -0500

    Added useContext

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,7 +1,12 @@
 import React, {useState} from 'react'
+import { Input, InputAdornment } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+
 
 const SearchBar = ({search, setSearch}) => (
-        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/>
+        <Input type="text" value={search} onChange={(e) => setSearch(e.target.value)} 
+        startAdornment={<InputAdornment><Search/></InputAdornment>}
+        />
 )
 
 export default SearchBar
