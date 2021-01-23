e87da7d1e61d155714927012669598c8b212c09e
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-22 23:59:06 -0500

    .

--- src/Containers/MovieContainer.js
+++ src/Containers/MovieContainer.js
@@ -1,12 +1,10 @@
 import React, {useState} from 'react'
-import SearchBar from '../Components/SearchBar.js'
 import Results from '../Components/Results.js'
 import Nominations from '../Components/Nominations.js'
 
 
 const MovieContainer = ({movieData})=> {
 
-    const [search, setSearch] = useState("")
     //useState is the hook
     //search and setSearch are state variables within the useState hook where search is the value and setSearch is a function that allows you change the value of search
 
@@ -14,11 +12,9 @@ const MovieContainer = ({movieData})=> {
         movieData.filter(el => el.Title.toLowerCase().includes(search.toLowerCase()))
     )
 
-    // console.log(movieFilter(), movieData[0].Title)
 
     return (
         <div>
-            <SearchBar search={search} setSearch={setSearch}/>
             <Results movieData={movieFilter()}/>
             <Nominations/>
 
