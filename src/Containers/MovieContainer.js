ea464d98223d9baf947b1b798acadbf416b97ec3
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-29 15:42:41 -0500

    .

--- src/Containers/MovieContainer.js
+++ src/Containers/MovieContainer.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Results from '../Components/Results.js'
 import Nominations from '../Components/Nominations.js'
 import SearchBar from '../Components/SearchBar.js'
@@ -6,13 +6,19 @@ import SearchBar from '../Components/SearchBar.js'
 const MovieContainer = ({movieData})=> {
 
     const [search, setSearch] = useState("")
-
+    
+    const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=117ef9f7"
     //useState is the hook
     //search and setSearch are state variables within the useState hook where search is the value and setSearch is a function that allows you change the value of search
 
     const movieFilter = () => (
         movieData.filter(el => el.Title.toLowerCase().includes(search.toLowerCase()))
     )
+    
+    useEffect(() => {
+        
+
+      }, [])
 
     return (
         <div>
